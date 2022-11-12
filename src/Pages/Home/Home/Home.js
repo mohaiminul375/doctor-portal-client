24c254ba7af136b9c8a390e62f29e578c1274ad6
mohaiminul375 <mohaiminul375@gmail.com>
2022-11-12 19:46:00 +0600

    add servicees

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import InfoCards from '../InfoCards/InfoCards';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div className='mx-6'>
            <Banner></Banner> 
            <InfoCards></InfoCards>
+           <Services></Services>
         </div>
     );
 };
