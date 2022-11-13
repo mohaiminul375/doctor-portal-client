7b941fa762843ab5541a376947bd846535af84f7
mohaiminul375 <mohaiminul375@gmail.com>
2022-11-13 18:20:52 +0600

    send data to modal

--- src/Pages/Appointment/AvilableAppointment/AppointmentCard.js
+++ src/Pages/Appointment/AvilableAppointment/AppointmentCard.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React from "react";
 
-const AppointmentCard = ({service}) => {
-    const {name,slots} = service;
-    return (
-        <div className="card shadow-xl">
-        <div className="card-body text-center">
-          <h2 className="card-title text-xl text-primary">{name}</h2>
-          <p>{slots.length > 0? slots[0] : 'Try another day' }</p>
-          <p>{slots.length} {slots.length > 1 ?'spaces' : 'space' } Available</p>
-          <div className="card-actions justify-center">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
+const AppointmentCard = ({ service, setTreatment }) => {
+  const { name, slots } = service;
+  return (
+    <div className="card shadow-xl">
+      <div className="card-body text-center">
+        <h2 className="text-xl font-bold text-secondary">{name}</h2>
+        <p>{slots.length > 0 ? slots[0] : "Try another day"}</p>
+        <p>
+          {slots.length} {slots.length > 1 ? "spaces" : "space"} Available
+        </p>
+        <div className="card-actions justify-center">
+          {/* <button className="btn ">Book Appointment</button> */}
+          <label
+           htmlFor="booking-modal"
+            className="btn btn-primary text-white"
+            onClick={()=>setTreatment(service)}
+            >
+            Book Appointment
+          </label>
         </div>
-      </div> 
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AppointmentCard;
--- src/Pages/Appointment/AvilableAppointment/AvilableAppointments.js
+++ src/Pages/Appointment/AvilableAppointment/AvilableAppointments.js
@@ -1,9 +1,11 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import BookingModal from '../../BookingModal/BookingModal';
 import AppointmentCard from './AppointmentCard';
 
 const AvailableAppointments = ({selectedDate}) => {
     const [services,setServices] = useState([]);
+    const [treatment,setTreatment] = useState(null);
     useEffect(()=>{
         fetch('Services.json')
         .then(res => res.json())
@@ -13,13 +15,20 @@ const AvailableAppointments = ({selectedDate}) => {
     return (
         <section className='mt-16'>
                 <h2 className='text-center text-secondary font-bold'>Available Appointment on {format(selectedDate,'PPPP')}</h2>
-                <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
                 {
                     services.map(service => <AppointmentCard
                     key={service._id}
                     service={service}
+                    setTreatment={setTreatment}
                     ></AppointmentCard> )
                 }
+                </div>
+                <div>
+                   {treatment && 
+                   <BookingModal
+                    treatment={treatment}
+                    ></BookingModal>}
                 </div>
         </section>
     );
