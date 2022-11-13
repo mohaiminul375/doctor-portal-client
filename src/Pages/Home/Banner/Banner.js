f81b0ab16a4ed8f5408689973b3813a72a8038aa
mohaiminul375 <mohaiminul375@gmail.com>
2022-11-13 07:06:41 +0600

    solve banner tailwind error

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,13 +3,13 @@ import chair from '../../../assets/images/chair.png'
 
 const Banner = () => {
     return (
-        <div className="hero  bg-base-200">
+        <div className="hero bg-base-100">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" alt='Bannerimage'/>
+          <img src={chair} className="w-1/2 rounded-lg shadow-2xl" alt='Bannerimage'/>
           <div>
             <h1 className="text-5xl font-bold">Box Office News!</h1>
             <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button className="btn btn-primary">Get Started</button>
+            <button className="btn btn-secondary-r to btn-primary ">Get Started</button>
           </div>
         </div>
       </div>
